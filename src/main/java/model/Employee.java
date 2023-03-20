168a4b13bf71c53e621db818113191fef2537ab6
phucgari <phucgari@gmail.com>
2023-03-20 08:44:40 +0700

    create model employee

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,70 @@
+package model;
+
+public class Employee {
+    private String name;
+    private String email;
+    private String address;
+    private long phoneNumber;
+    private long salary;
+    private String department;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String email, String address, long phoneNumber, long salary, String department) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
